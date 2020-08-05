cd88ca834d15436585d5b243d5958230179ab8b5
Ben Sloane <benjamin.sloane@sas.com>
2020-08-05 17:04:52 -0400

    Activation functions completed

--- src/activations.c
+++ src/activations.c
@@ -14,18 +14,29 @@ void activate_matrix(matrix m, ACTIVATION a)
         for(j = 0; j < m.cols; ++j){
             double x = m.data[i*m.cols + j];
             if(a == LOGISTIC){
-                // TODO
+		m.data[i*m.cols + j] = 1 / (1 + exp(-1 * x));
             } else if (a == RELU){
-                // TODO
+		if (x <= 0){
+		    m.data[i*m.cols + j] = 0;	
+		} else {	
+		    m.data[i*m.cols + j] = x;
+		} 
             } else if (a == LRELU){
-                // TODO
-            } else if (a == SOFTMAX){
-                // TODO
+            	if (x < 0){
+		    m.data[i*m.cols + j] = 0.01 * x;	
+		} else {	
+		    m.data[i*m.cols + j] = x;
+		} 
+	    } else if (a == SOFTMAX){
+		 m.data[i*m.cols + j] = exp(x);
             }
             sum += m.data[i*m.cols + j];
         }
         if (a == SOFTMAX) {
-            // TODO: have to normalize by sum if we are using SOFTMAX
+            // Have to normalize by sum if we are using SOFTMAX
+	    for(j = 0; j < m.cols; ++j){
+	    	m.data[i*m.cols + j] /= sum;
+	    }
         }
     }
 }
@@ -42,6 +53,23 @@ void gradient_matrix(matrix m, ACTIVATION a, matrix d)
         for(j = 0; j < m.cols; ++j){
             double x = m.data[i*m.cols + j];
             // TODO: multiply the correct element of d by the gradient
-        }
+       	    if (a == LOGISTIC) {
+	    	d.data[i * d.cols + j] *= x * (1 - x); 
+	    } else if (a == RELU) {
+	   	if (x <= 0) {
+		    d.data[i * d.cols + j] *= 0; 
+		} else {
+		    d.data[i * d.cols + j] *= 1;		
+		}	
+	    } else if (a == LRELU) {
+	   	if (x < 0) {
+		    d.data[i * d.cols + j] *= 0.01; 
+		} else {
+		    d.data[i * d.cols + j] *= 1;		
+		}	
+	    } else if (a == SOFTMAX) {
+	   	d.data[i * d.cols + j] *= 1;  
+	    } 
+	}
     }
 }
